68c11f6a19cd74179f1d5bc732709eabfd1fe206
ggiovadam <guido.giovadam@gmail.com>
2025-04-14 14:43:54 +0200

    feat: created basic structure
    Created the basic structure with Spring Initializr

---
+++ src/main/java/es/daw2/fct_fct/FctFctApplication.java
@@ -0,0 +1,13 @@
+package es.daw2.fct_fct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FctFctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FctFctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw2.fct_fct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FctFctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
