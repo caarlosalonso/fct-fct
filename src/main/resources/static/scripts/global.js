1cbe0d323d3c9ca9c32394c147d0dc9fcd49e648
ggiovadam <guido.giovadam@gmail.com>
2025-05-06 13:26:34 +0200

    feat: add automatic password visibility switch
    * Added password visibility switch.
    * Allows the password to be hidden or shown.
    * Automatically gets added if there is a password input

--- src/main/resources/static/scripts/global.js
+++ src/main/resources/static/scripts/global.js
@@ -1,1 +1,29 @@
-// Empty for now
+document.addEventListener('DOMContentLoaded', (event) => {
+    addVisibilityIconPasswords();
+});
+
+function addVisibilityIconPasswords() {
+    document.querySelectorAll('input[type="password"]')
+    .forEach(input => {
+        const wrapper = input.parentNode;
+        if (! wrapper.classList.contains('wrapper-password')) {
+            wrapper.classList.add('wrapper-password');
+        }
+
+        const icon = document.createElement('img');
+        icon.classList.add('password-icon');
+        icon.src = '../static/images/eye.svg';
+        icon.alt = 'Toggle password visibility';
+        wrapper.appendChild(icon);
+
+        icon.addEventListener('click', () => {
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.src = '../static/images/eye-crossed.svg';
+            } else {
+                input.type = 'password';
+                icon.src = '../static/images/eye.svg';
+            }
+        });
+    });
+}
