59cc21f40c99f3eeb6e74d98ed8b896a0b08e119
caarlosalonso <calonsocubillo@gmail.com>
2025-05-26 21:12:52 +0200

    refactor: testing for the deployment

--- src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
+++ src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
@@ -1,6 +1,7 @@
 package es.daw2.fct_fct;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,10 @@ class FctFctApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	public static void main(String[] args) {
+		System.out.println("Puerto detectado: " + System.getenv("PORT"));
+		SpringApplication.run(FctFctApplication.class, args);
 	}
 
 }
