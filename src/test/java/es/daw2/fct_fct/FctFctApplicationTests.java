6136e37577ec32abc9820a966c024c103f3e6585
caarlosalonso <calonsocubillo@gmail.com>
2025-05-26 21:40:33 +0200

    refactor: added a debug print

--- src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
+++ src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
@@ -1,7 +1,6 @@
 package es.daw2.fct_fct;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,9 +9,4 @@ class FctFctApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	public static void main(String[] args) {
-		System.out.println("Puerto detectado: " + System.getenv("PORT"));
-		SpringApplication.run(FctFctApplication.class, args);
-	}
-
 }
