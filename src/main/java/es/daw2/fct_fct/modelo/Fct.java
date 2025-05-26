03bc82f069838a95382c84930d5ce92483e7e2ab
caarlosalonso <calonsocubillo@gmail.com>
2025-05-27 00:47:09 +0200

    fix: changed table name

--- src/main/java/es/daw2/fct_fct/modelo/Fct.java
+++ src/main/java/es/daw2/fct_fct/modelo/Fct.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "alumnos_has_ciclos")
+@Table(name = "cursos")
 public class Fct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
