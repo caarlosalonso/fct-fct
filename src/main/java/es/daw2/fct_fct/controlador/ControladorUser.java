88575016c4a3edb48fb8aaf6e1570c7e741b219b
caarlosalonso <calonsocubillo@gmail.com>
2025-05-27 15:35:04 +0200

    feat: Sets the base path for the controllers endpoints

--- src/main/java/es/daw2/fct_fct/controlador/ControladorUser.java
+++ src/main/java/es/daw2/fct_fct/controlador/ControladorUser.java
@@ -43,6 +43,7 @@ public class ControladorUser {
     }
 
     //Crud
+    @RequestMapping("/api/users") //Define la ruta base para los endpoints del controlador
     @PostMapping("/create")
     public ResponseEntity<?> crearAlumno(@RequestBody User u) {
         servicioUser.addUsers(u);
