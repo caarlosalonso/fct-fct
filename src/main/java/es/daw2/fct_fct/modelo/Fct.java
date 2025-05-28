1bad13cfd957a0f7f55da98832f767c73821ca02
ggiovadam <guido.giovadam@gmail.com>
2025-05-28 12:47:41 +0200

    fix: correct name for table in fct

--- src/main/java/es/daw2/fct_fct/modelo/Fct.java
+++ src/main/java/es/daw2/fct_fct/modelo/Fct.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "cursos")
+@Table(name = "fcts")
 public class Fct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
