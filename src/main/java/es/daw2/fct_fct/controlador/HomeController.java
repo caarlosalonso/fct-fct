82d890974c400fcb6cb8460086acf13f6430a67b
ggiovadam <guido.giovadam@gmail.com>
2025-05-29 09:27:25 +0200

    clean: remove unused import from HomeController

--- src/main/java/es/daw2/fct_fct/controlador/HomeController.java
+++ src/main/java/es/daw2/fct_fct/controlador/HomeController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @Controller
