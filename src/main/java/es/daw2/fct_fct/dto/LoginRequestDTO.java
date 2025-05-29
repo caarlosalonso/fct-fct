577c8ea8c2c2286190f89f841fccba8fe49bdb9d
caarlosalonso <calonsocubillo@gmail.com>
2025-05-30 00:01:26 +0200

    feat: added getters

--- src/main/java/es/daw2/fct_fct/dto/LoginRequestDTO.java
+++ src/main/java/es/daw2/fct_fct/dto/LoginRequestDTO.java
@@ -1,3 +1,12 @@
 package es.daw2.fct_fct.dto;
 
-public record LoginRequestDTO(String email, String password) {}
+public record LoginRequestDTO(String email, String password) {
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
