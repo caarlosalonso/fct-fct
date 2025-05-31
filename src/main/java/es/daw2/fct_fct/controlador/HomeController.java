572c63d85e650e3036f621384c0cda9af9e6a075
ggiovadam <guido.giovadam@gmail.com>
2025-05-31 16:19:45 +0200

    feat: enum with the different pages

--- src/main/java/es/daw2/fct_fct/controlador/HomeController.java
+++ src/main/java/es/daw2/fct_fct/controlador/HomeController.java
@@ -9,26 +9,64 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class HomeController {
 
+    private enum PAGES {
+    /*  Authentication                                                          */
+        LOGIN("/auth/login.html"),
+        REDIRECT_LOGIN("redirect:/auth/login.html"),
+    /*  Errors                                                                  */
+        ERROR("/error.html"),
+    /*  User Management                                                         */
+    /*  Admin                                                                   */
+        ADMIN("/admin.html"),
+    /*  Coordinación                                                            */
+        COORDINACION("/coordinacion.html"),
+    /*  Tutor                                                                   */
+        TUTOR("/tutor.html"),
+    /*  Alumno                                                                  */
+        ALUMNO("/alumno.html"),
+
+
+
+
+    /*
+Do not disturb the sacred semicolon's deep slumber.
+    *  \   |*  /     *
+      * \  |  /  *          *
+ *   --- */;/* ---      *
+   *    /  |  \   *           *
+       / * |   \      *
+*/
+        private final String path;
+
+        PAGES(String path) {
+            this.path = path;
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
+
     @GetMapping("/")
     public String index(HttpSession session) {
         if (session.getAttribute("user") == null)
-            return "redirect:/login";
+            return PAGES.REDIRECT_LOGIN.getPath();
         
         if (session.getAttribute("role") == null)
-            return "redirect:/login";
+            return PAGES.LOGIN.getPath();
 
         return switch (session.getAttribute("role").toString()) {
-            case "admin"        ->  "admin.html";
-            case "coordinador"  ->  "coordinacion.html";
-            case "tutor"        ->  "tutor.html";
-            case "alumno"       ->  "alumno.html";
-            default             ->  "login.html";
+            case "admin"        ->  PAGES.ADMIN.getPath();
+            case "coordinador"  ->  PAGES.COORDINACION.getPath();
+            case "tutor"        ->  PAGES.TUTOR.getPath();
+            case "alumno"       ->  PAGES.ALUMNO.getPath();
+            default             ->  PAGES.LOGIN.getPath();
         };
     }
 
     @GetMapping("/login")
     public String login() {
-        return "login.html";
+        return PAGES.LOGIN.getPath();
     }
 
     @GetMapping("/crear")
@@ -38,12 +76,12 @@ public class HomeController {
 
     @GetMapping("/error")
     public String error() {
-        return "error.html";
+        return PAGES.ERROR.getPath();
     }
 
     @GetMapping("/coordinacion")
     public String coordinacion() {
-        return "coordinacion.html";
+        return PAGES.COORDINACION.getPath();
     }
 
     @GetMapping("/index")
