7dee5f66f32835dc153e8ebb3d66b304964b7db7
caarlosalonso <calonsocubillo@gmail.com>
2025-06-02 21:06:49 +0200

    fix: @Service was missing :S fingers crossed

--- src/main/java/es/daw2/fct_fct/servicio/servicioVAlumno.java
+++ src/main/java/es/daw2/fct_fct/servicio/servicioVAlumno.java
@@ -2,10 +2,12 @@ package es.daw2.fct_fct.servicio;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 import es.daw2.fct_fct.modelo.vAlumno;
 import es.daw2.fct_fct.repositorio.RepositorioVAlumno;
 
+@Service
 public class servicioVAlumno extends AbstractService<Long, vAlumno, RepositorioVAlumno> {
 
     public Optional<vAlumno> getByUserId(Long id) {
