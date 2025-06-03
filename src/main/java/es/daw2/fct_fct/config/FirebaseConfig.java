ce5c62197863e076a71b0630c20d9481e258ee11
caarlosalonso <calonsocubillo@gmail.com>
2025-06-03 23:17:33 +0200

    refactor: excluded firebase config from test

--- src/main/java/es/daw2/fct_fct/config/FirebaseConfig.java
+++ src/main/java/es/daw2/fct_fct/config/FirebaseConfig.java
@@ -3,10 +3,12 @@ package es.daw2.fct_fct.config;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -15,8 +17,17 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig {
 
+    private final Environment env;
+
+    public FirebaseConfig(Environment env) {
+        this.env = env;
+    }
+
     @PostConstruct
     public void init() throws Exception {
+
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) return;
+
         String credJson = System.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON");
         if (credJson == null || credJson.isEmpty()) {
             throw new IllegalStateException("GOOGLE_APPLICATION_CREDENTIALS_JSON env variable not set or empty");
