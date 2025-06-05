2ba4da0b98889bfa93f76e0a95f4806fb3f0c68a
caarlosalonso <calonsocubillo@gmail.com>
2025-06-05 22:00:26 +0200

    refactor: test 7

--- src/main/java/es/daw2/fct_fct/FctFctApplication.java
+++ src/main/java/es/daw2/fct_fct/FctFctApplication.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.daw2.fct_fct.utils.EnvLoader;
-
 @SpringBootApplication
 public class FctFctApplication {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Puerto detectado: " + System.getenv("PORT"));
-		EnvLoader.loadEnv(".env");
 		SpringApplication.run(FctFctApplication.class, args);
 		
 	}
--- src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
+++ src/test/java/es/daw2/fct_fct/FctFctApplicationTests.java
@@ -7,11 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FctFctApplicationTests {
 
-	@BeforeAll
-	static void setup() {
-		System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "src/test/resources/firebase-key.json");
-	}
-	
 	@Test
 	void contextLoads() {
 	}
