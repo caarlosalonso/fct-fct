4781a99a82360d145f0faad80f5024832342ab0c
ggiovadam <guido.giovadam@gmail.com>
2025-06-06 18:10:22 +0200

    feat: select has dropdown

--- src/main/resources/static/scripts/classes/SelectInput.js
+++ src/main/resources/static/scripts/classes/SelectInput.js
@@ -4,27 +4,77 @@ export class SelectInput extends TextInput {
     constructor(input) {
         super(input);
         this.options = [];
-        this.input.value = null;
+        this.dropdown = null;
+        this.input.value = '';
         this.validate = () => {
             if (this.isEmpty()) return true;
 
             const selectedValue = this.input.value;
             return this.options.some(option => option.value === selectedValue);
-        }
+        };
     }
 
     init() {
         super.init();
         this.buildSelect();
+        this.createDropdown();
     }
 
     buildSelect() {
         const options = this.input.getAttribute('data-options');
         if (!options) return;
 
-        options.split(',').forEach(option => {
+        options.split(';').forEach(option => {
             const [value, label] = option.split(':');
             this.options.push({ value, label });
         });
+    }
+
+    createDropdown() {
+        this.dropdown = document.createElement('div');
+        this.dropdown.classList.add('dropdown', 'collapsed');
+        this.parent.appendChild(this.dropdown);
+
+        this.options.forEach(({ value, label }) => {
+            const optionElement = document.createElement('div');
+            optionElement.classList.add('dropdown-option');
+            optionElement.textContent = label;
+
+            optionElement.addEventListener('click', () => {
+                this.input.value = value;
+                this.input.dispatchEvent(new Event('input'));
+                this.hideDropdown();
+            });
+
+            this.dropdown.appendChild(optionElement);
+        });
+
+        // Show dropdown on focus
+        this.input.addEventListener('focus', () => {
+            this.showDropdown();
+        });
+
+        // Hide dropdown on blur
+        this.input.addEventListener('blur', () => {
+            this.hideDropdown();
+        });
+    }
+
+    showDropdown() {
+        this.input.style.borderBottomLeftRadius = '0px';
+        this.input.style.borderBottomRightRadius = '0px';
+        this.dropdown.style.display = "block";
+        this.dropdown.style.height = `${Math.min(this.dropdown.scrollHeight, 200)}px`;
+        this.dropdown.style.overflowY = "auto";
+    }
+
+    hideDropdown() {
+        this.dropdown.style.height = '0';
+        this.dropdown.style.overflowY = 'hidden';
+        setTimeout(() => {
+            this.input.style.borderBottomLeftRadius = this.input.style.borderTopRightRadius;
+            this.input.style.borderBottomRightRadius = this.input.style.borderTopRightRadius;
+            this.dropdown.style.display = "none";
+        }, 200);
     }
 }
