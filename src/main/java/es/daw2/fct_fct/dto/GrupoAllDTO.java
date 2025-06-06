8442c751d17a3dceeffed846d0e4d1079e58dfc6
ggiovadam <guido.giovadam@gmail.com>
2025-06-07 00:46:51 +0200

    fix: names in DTO

--- src/main/java/es/daw2/fct_fct/dto/GrupoAllDTO.java
+++ src/main/java/es/daw2/fct_fct/dto/GrupoAllDTO.java
@@ -1,3 +1,3 @@
 package es.daw2.fct_fct.dto;
 
-public record GrupoAllDTO(Long id, Long ciclo, Long cicloLectivo, Short numero, String horario) {}
+public record GrupoAllDTO(Long id, Long cicloId, Long cicloLectivoId, Short numero, String horario) {}
