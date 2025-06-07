045e727cb92f6760977b1647c398e86c9de84992
ggiovadam <guido.giovadam@gmail.com>
2025-06-07 20:46:37 +0200

    feat: show all

--- src/main/resources/static/scripts/coordinacion/alumnado.js
+++ src/main/resources/static/scripts/coordinacion/alumnado.js
@@ -50,7 +50,7 @@ function build(ciclosLectivos, gruposCiclos, alumnos) {
             updatedAt: cicloLectivo.updatedAt,
             nombre: cicloLectivo.nombre,
             fechaInicio: cicloLectivo.fechaInicio,
-            grupo: gruposCiclos.filter(grupo => grupo.cicloLectivoId === cicloLectivo.id).map(grupo => {
+            grupos: gruposCiclos.filter(grupo => grupo.cicloLectivoId === cicloLectivo.id).map(grupo => {
                 return {
                     cicloLectivoId: grupo.cicloLectivoId,
                     grupoId: grupo.grupoId,
@@ -62,4 +62,66 @@ function build(ciclosLectivos, gruposCiclos, alumnos) {
         })
     });
     console.log(info);
+}
+
+function createCiclosLectivos() {
+    const cicloLectivoSelection = document.getElementById('ciclo-lectivo-selection');
+    cicloLectivoSelection.innerHTML = '';
+
+    const ul = document.createElement('ul');
+    ul.classList.add('ciclos-lectivos', 'nav', 'nav-tabs');
+    ul.id = 'ciclos-lectivos';
+    cicloLectivoSelection.appendChild(ul);
+
+    info.forEach((cicloLectivo) => {
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.textContent = cicloLectivo.nombre;
+        ul.appendChild(li);
+
+        li.addEventListener('click', () => {
+            createGruposCiclos(cicloLectivo.id);
+        });
+    });
+}
+
+function createGruposCiclos(cicloLectivoId) {
+    const gruposCiclosSelection = document.getElementById('grupos-ciclos-selection');
+    gruposCiclosSelection.innerHTML = '';
+
+    const ul = document.createElement('ul');
+    ul.classList.add('grupos-ciclos', 'nav', 'nav-tabs');
+    ul.id = 'grupos-ciclos';
+    gruposCiclosSelection.appendChild(ul);
+
+    const cicloLectivo = info.find(ciclo => ciclo.id === cicloLectivoId);
+    if (cicloLectivo) {
+        cicloLectivo.grupos.forEach((grupo) => {
+            const li = document.createElement('li');
+            li.classList.add('nav-item');
+            li.textContent = grupo.grupo_nombre;
+            ul.appendChild(li);
+
+            li.addEventListener('click', () => {
+                createAlumnos(grupo.alumnos);
+            });
+        });
+    }
+}
+
+function createAlumnos(alumnos) {
+    const alumnosSelection = document.getElementById('alumnos-selection');
+    alumnosSelection.innerHTML = '';
+
+    const ul = document.createElement('ul');
+    ul.classList.add('alumnos', 'nav', 'nav-tabs');
+    ul.id = 'alumnos';
+    alumnosSelection.appendChild(ul);
+
+    alumnos.forEach((alumno) => {
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.textContent = `${alumno.nombre} ${alumno.apellido}`;
+        ul.appendChild(li);
+    });
 }
