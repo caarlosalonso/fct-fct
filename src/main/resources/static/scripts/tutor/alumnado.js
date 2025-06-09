ffd2ea5db1bf60b52dcbf3842a0d4875d0298a16
ggiovadam <guido.giovadam@gmail.com>
2025-06-10 01:02:29 +0200

    style: fix styling

--- src/main/resources/static/scripts/tutor/alumnado.js
+++ src/main/resources/static/scripts/tutor/alumnado.js
@@ -178,26 +178,26 @@ function crearLista(alumnosCurso, grupoTutor, form) {
 
     alumnosCurso.forEach(alumno => {
         const item = document.createElement('div');
-        item.classList.add('alumno-item');
+        item.classList.add('cell-value', 'alumno-item');
         listar.appendChild(item);
 
         const titleSpan = document.createElement('span');
-        titleSpan.classList.add('cell-title');
+        titleSpan.classList.add('cell-value', 'cell-title');
         titleSpan.textContent = alumno.nombreAlumno;
         item.appendChild(titleSpan);
 
         const niaSpan = document.createElement('span');
-        niaSpan.classList.add('cell-subtitle');
+        niaSpan.classList.add('cell-value', 'cell-subtitle');
         niaSpan.textContent = `NIA: ${alumno.nia}`;
         item.appendChild(niaSpan);
 
         const dniSpan = document.createElement('span');
-        dniSpan.classList.add('cell-subtitle');
+        dniSpan.classList.add('cell-value', 'cell-subtitle');
         dniSpan.textContent = `DNI: ${alumno.dni}`;
         item.appendChild(dniSpan);
 
         const emailSpan = document.createElement('span');
-        emailSpan.classList.add('cell-subtitle');
+        emailSpan.classList.add('cell-value', 'cell-subtitle');
         emailSpan.textContent = `Email: ${alumno.email}`;
         item.appendChild(emailSpan);
 
@@ -206,7 +206,8 @@ function crearLista(alumnosCurso, grupoTutor, form) {
                 '0 -0.5 25 25',
                 'M 20.848 1.879 C 19.676 0.707 17.777 0.707 16.605 1.879 L 2.447 16.036 C 2.029 16.455 1.743 16.988 1.627 17.569 L 1.04 20.505 C 0.76 21.904 1.994 23.138 3.393 22.858 L 6.329 22.271 C 6.909 22.155 7.443 21.869 7.862 21.451 L 22.019 7.293 C 23.191 6.121 23.191 4.222 22.019 3.05 L 20.848 1.879 Z M 18.019 3.293 C 18.41 2.902 19.043 2.902 19.433 3.293 L 20.605 4.465 C 20.996 4.855 20.996 5.488 20.605 5.879 L 6.447 20.036 C 6.308 20.176 6.13 20.271 5.936 20.31 L 3.001 20.897 L 3.588 17.962 C 3.627 17.768 3.722 17.59 3.862 17.451 L 13.933 7.379 L 16.52 9.965 L 17.934 8.56 L 15.348 5.965 L 18.019 3.293 Z',
                 () => setInputsToUpdate(form, alumno),
-                'edit-svg'
+                'edit-svg',
+                'svg'
             )
         );
         item.appendChild(
@@ -217,7 +218,8 @@ function crearLista(alumnosCurso, grupoTutor, form) {
                     event.preventDefault();
                     removeAlumnoFromGrupo(form, alumno.alumnoId, grupoTutor.grupoId);
                 },
-                'delete-svg'
+                'delete-svg',
+                'svg'
             )
         );
         item.appendChild(
@@ -228,7 +230,8 @@ function crearLista(alumnosCurso, grupoTutor, form) {
                     event.preventDefault();
                     resetPassword(form, alumno.alumnoId);
                 },
-                'password-reset-svg'
+                'password-reset-svg',
+                'svg'
             )
         );
     });
