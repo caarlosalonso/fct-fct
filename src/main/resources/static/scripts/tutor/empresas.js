335e87b701478559be4fc4fc4eea7d86176df962
caarlosalonso <calonsocubillo@gmail.com>
2025-06-10 00:27:12 +0200

    fixed:

--- src/main/resources/static/scripts/tutor/empresas.js
+++ src/main/resources/static/scripts/tutor/empresas.js
@@ -304,6 +304,7 @@ propuesta_por: parseInt(form.getInput('propuesta_por').getValue(), 10),         
             form.showError('Error al actualizar la empresa');
         });
     };
+}
 
 function removeEmpresa(empresa) {
     if (!confirm(`¿Estás seguro de que quieres eliminar la empresa "${empresa.nombre}"?`)) {
