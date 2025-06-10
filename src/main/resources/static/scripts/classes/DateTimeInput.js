1d4f49987992a618ab437df4c0b5231a00fad60b
ggiovadam <guido.giovadam@gmail.com>
2025-06-10 12:05:04 +0200

    test: toIsoString

--- src/main/resources/static/scripts/classes/DateTimeInput.js
+++ src/main/resources/static/scripts/classes/DateTimeInput.js
@@ -12,7 +12,7 @@ export class DateTimeInput extends TextInput {
         this.validate = () => {
             if (this.isEmpty()) return true;
             const date = new Date(this.input.value);
-            console.log(date);
+            console.log(date.toISOString());
             return !isNaN(date.getTime());
         };
     }
