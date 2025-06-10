85fdf0334d02878e6c0d32b84e87d90888b11c80
ggiovadam <guido.giovadam@gmail.com>
2025-06-10 18:03:17 +0200

    test: hopefully this is a fix

--- src/main/resources/static/scripts/classes/SelectInput.js
+++ src/main/resources/static/scripts/classes/SelectInput.js
@@ -104,9 +104,10 @@ export class SelectInput extends TextInput {
         while(this.dropdown && this.dropdown.firstChild) this.dropdown.removeChild(this.dropdown.firstChild);
         this.options = (!array || !Array.isArray(array)) ? [] : array;
         this.createOptions();
-        this.dropdown.style.height = '0';
+        this.dropdown.style.height = "auto";
         this.dropdown.style.overflowY = 'hidden';
         this.dropdown.style.display = "none";
+        this.dropdown.style.height = '0px';
 
         if (interacting) {
             this.showDropdown();
