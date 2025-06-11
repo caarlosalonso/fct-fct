d289b72a54b0b77ae890ffca612c538ca6f24407
ggiovadam <guido.giovadam@gmail.com>
2025-06-11 16:51:37 +0200

    feat: add reviews js to alumno

---
+++ src/main/resources/static/scripts/alumno/reviews.js
@@ -0,0 +1,32 @@
+import { Form } from '../classes/Form.js';
+import { tableLoading, tableFail, createSVG, createClickableSVG } from '../functions.js';
+
+const SECTION = 'reviews-section';
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    promise();
+});
+
+function promise() {
+    Promise.all([
+        fetchReviews()
+    ])
+    .then(([
+        reviews
+    ]) => {
+        build(reviews);
+    }).catch((error) => {
+        console.error('Error al obtener las empresas:', error);
+    });
+}
+
+async function fetchEmpresas() {
+    const response = await fetch('/api/vista-empresas-plazas/all');
+    if (response.status === 204) return [];
+    if (!response.ok) throw new Error('Error al obtener las empresas');
+    return await response.json();
+}
+
+function build(empresas) {
+    console.log('Empresas:', empresas);
+}
