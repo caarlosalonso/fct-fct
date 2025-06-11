4421b7be63d4b174ad82adc415b412181f0d2bad
caarlosalonso <calonsocubillo@gmail.com>
2025-06-11 14:05:25 +0200

    fix: please

--- src/main/resources/static/scripts/tutor/empresas.js
+++ src/main/resources/static/scripts/tutor/empresas.js
@@ -304,7 +304,7 @@ function editEmpresa(empresa) {
             nombre_usuario: empresa.nombre_usuario || null
         };
 
-        fetch(`/api/vista-empresas-tutores/${empresa.empresa.id}`, {
+        fetch(`/api/vista-empresas-tutores/${empresa.empresa.Id || empresa.id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
