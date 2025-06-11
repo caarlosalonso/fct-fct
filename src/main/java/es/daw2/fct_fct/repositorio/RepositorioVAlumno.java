76c8b9499e850cf886c12a2cc237954c6d301058
caarlosalonso <calonsocubillo@gmail.com>
2025-06-11 23:36:32 +0200

    fix: get the fk out

--- src/main/java/es/daw2/fct_fct/repositorio/RepositorioVAlumno.java
+++ src/main/java/es/daw2/fct_fct/repositorio/RepositorioVAlumno.java
@@ -1,10 +1,9 @@
 package es.daw2.fct_fct.repositorio;
 
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import es.daw2.fct_fct.modelo.vAlumno;
 
 public interface RepositorioVAlumno extends CrudRepository<vAlumno, Long>{
-    Optional<vAlumno> findById(Long id);
+
 }
