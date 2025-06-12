1910fa86ffc44999f182e80a998d6fd338596b4c
ggiovadam <guido.giovadam@gmail.com>
2025-06-12 10:04:43 +0200

    feat: remove pings

--- src/main/java/es/daw2/fct_fct/servicio/ServicioArchivo.java
+++ src/main/java/es/daw2/fct_fct/servicio/ServicioArchivo.java
@@ -29,50 +29,34 @@ public class ServicioArchivo extends AbstractService<Long, User, RepositorioUser
     @Autowired
     private ServicioAlumno servicioAlumno;
     @Autowired
-    private RepositorioCicloLectivo repositorioCicloLectivo;
-    @Autowired
-    private RepositorioCiclos repositorioCiclos;
-    @Autowired
-    private RepositorioGrupos repositorioGrupos;
-    @Autowired
     private RepositorioCurso repositorioCurso;
     @Autowired
     private ServicioCicloLectivo servicioCicloLectivo;
 
     public String subirArchivo(Long id, MultipartFile archivo) throws IOException {
         String bucketName = StorageClient.getInstance().bucket().getName();
-        System.out.println("Ping: 1: " + bucketName);
 
         Optional<User> va = repository.findById(id);
-        System.out.println("Ping: 2: " + va);
 
         if (va.isEmpty()) throw new IllegalArgumentException("El usuario no es un alumno válido");
-        System.out.println("Ping: 3: " + va.isEmpty());
 
         User user = va.get();
         Alumno al = servicioAlumno.getByUserId(user.getId())
             .orElseThrow(() -> new IllegalArgumentException("El usuario no es un alumno válido"));
 
-        System.out.println("Ping: 4: " + al);
         List<Curso> cursos = ((List<Curso>) repositorioCurso.findAll())
             .stream()
             .filter((curso) -> curso.getAlumno().getId() == al.getId())
             .toList();
-        System.out.println("Ping: 5: " + cursos);
 
         if (cursos.isEmpty()) throw new IllegalArgumentException("El alumno no tiene cursos asociados");
-
-        System.out.println("Ping: 6: " + cursos.isEmpty());
 
         Curso curso = cursos.get(0);
         Grupo grupo = curso.getGrupo();
         Ciclo ciclo = grupo.getCiclo();
-        
-        System.out.println("Ping: 7: " + curso + " " + grupo + " " + ciclo);
+
         CicloLectivo cicloLectivoActual = servicioCicloLectivo.getCicloLectivoActual()
             .orElseThrow(() -> new IllegalArgumentException("No hay ciclo lectivo actual"));
-
-        System.out.println("Ping: 8: " + cicloLectivoActual);
 
         String ruta = String.format("%d/%s/%d/%s/%s",
             cicloLectivoActual.getFechaInicio().getYear(),
@@ -81,13 +65,10 @@ public class ServicioArchivo extends AbstractService<Long, User, RepositorioUser
             user.getName(),
             archivo.getOriginalFilename());
 
-        System.out.println("Ping: 9: " + ruta);
-
         var blob = StorageClient.getInstance()
                                 .bucket()
                                 .create(ruta, archivo.getBytes(), archivo.getContentType());
 
-        System.out.println("Ping: 10: " + blob);
         return String.format(
             "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
             bucketName,
