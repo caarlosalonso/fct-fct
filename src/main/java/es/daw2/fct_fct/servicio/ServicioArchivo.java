3fed9169926a29d6ef36f18773714919b205c3a3
ggiovadam <guido.giovadam@gmail.com>
2025-06-12 09:30:00 +0200

    test: souts galore

--- src/main/java/es/daw2/fct_fct/servicio/ServicioArchivo.java
+++ src/main/java/es/daw2/fct_fct/servicio/ServicioArchivo.java
@@ -40,39 +40,54 @@ public class ServicioArchivo extends AbstractService<Long, User, RepositorioUser
     private ServicioCicloLectivo servicioCicloLectivo;
 
     public String subirArchivo(Long id, MultipartFile archivo) throws IOException {
-
         String bucketName = StorageClient.getInstance().bucket().getName();
+        System.out.println("Ping: 1: " + bucketName);
+
         Optional<User> va = repository.findById(id);
+        System.out.println("Ping: 2: " + va);
 
         if (va.isEmpty()) throw new IllegalArgumentException("El usuario no es un alumno válido");
+        System.out.println("Ping: 3: " + va.isEmpty());
 
         User user = va.get();
         Alumno al = repositorioAlumno.findById(user.getId())
             .orElseThrow(() -> new IllegalArgumentException("El usuario no es un alumno válido"));
 
+        System.out.println("Ping: 4: " + al);
         List<Curso> cursos = ((List<Curso>) repositorioCurso.findAll())
             .stream()
             .filter((curso) -> curso.getAlumno().getId() == al.getId())
             .toList();
+        System.out.println("Ping: 5: " + cursos);
 
         if (cursos.isEmpty()) throw new IllegalArgumentException("El alumno no tiene cursos asociados");
+
+        System.out.println("Ping: 6: " + cursos.isEmpty());
 
         Curso curso = cursos.get(0);
         Grupo grupo = curso.getGrupo();
         Ciclo ciclo = grupo.getCiclo();
+        
+        System.out.println("Ping: 7: " + curso + " " + grupo + " " + ciclo);
         CicloLectivo cicloLectivoActual = servicioCicloLectivo.getCicloLectivoActual()
             .orElseThrow(() -> new IllegalArgumentException("No hay ciclo lectivo actual"));
 
-        String ruta = String.format("%d/%s/%d/%s",
+        System.out.println("Ping: 8: " + cicloLectivoActual);
+
+        String ruta = String.format("%d/%s/%d/%s/%s",
             cicloLectivoActual.getFechaInicio().getYear(),
             ciclo.getAcronimo(),
             grupo.getNumero(),
-            user.getName());
+            user.getName(),
+            archivo.getOriginalFilename());
+
+        System.out.println("Ping: 9: " + ruta);
 
         var blob = StorageClient.getInstance()
                                 .bucket()
                                 .create(ruta, archivo.getBytes(), archivo.getContentType());
 
+        System.out.println("Ping: 10: " + blob);
         return String.format(
             "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media",
             bucketName,
